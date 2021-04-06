5ec6fedf7706cbd607d20053460b9bcf182b5143
Rashmi Deshmukh <57807833+rashmi-deshmukh@users.noreply.github.com>
2021-04-07 02:44:03 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,25 @@
+class launcher{
+	constructor(body,anchor)
+	{
+		
+		
+	}
+
+    fly(){
+
+        //set the bodyA of launcher to null
+
+
+    }
+
+    attach(body){
+        //set bodyA of the constrain to body
+
+        
+    }
+
+	display()
+	{
+
+    }
+}
